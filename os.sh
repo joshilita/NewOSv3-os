943d5cdf5bb83c4f4dfe7d4b206cb14b90b54783
joshilita <52619112+joshilita@users.noreply.github.com>
2021-11-07 12:23:03 -0500

    Add files via upload

---
+++ os.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+GREENFG="\e[38;5;82m"
+REDWEAKFG="\e[38;5;132m"
+BBLUEFG="\e[1;38;5;75m"
+GBLUEFG="\e[1;38;5;73m"
+FOLDERNAMEFG="\e[1;38;5;45m"
+ERRORFG="\e[1;38;5;197m"
+RESET="\e[0m"
+BOLD="\e[1m"
+
+echo -e "${BBLUEFG}NewOS V3 who dis?"
