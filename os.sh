327956da8712dd759da92d2f3c70e5f476bf7c59
joshilita <52619112+joshilita@users.noreply.github.com>
2021-11-11 09:02:16 -0500

    Update os.sh

--- os.sh
+++ os.sh
@@ -109,8 +109,8 @@ echo -e "${GREENFG}Bye!${RESET}"
 exit 0
 elif [ "$input" = "changelog" ]; then
 echo -e $(curl -s 'https://raw.githubusercontent.com/joshilita/NewOSV3/main/changelog.txt')
-elif [ "$input" = ^[[A^[[A^[[B^[[B^[[D^[[D^[[C^[[C^[[A^[[A^[[B^[[B^[[D^[[D^[[C^[[C ]; then
-echo "cool guy pogjoishgdgidu"
+elif [ "$input" = ""]; then
+echo "type something"
 else
 echo -e "${ERRORFG}(${input})Command not found.${RESET}"
 fi
